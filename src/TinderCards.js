5444b5d36d1e491dcb1e5a132c246ed8ff49daa3
dhruv singhal <dhruv.singhal2612@gmail.com>
2021-04-11 18:12:56 +0530

    added nodejs mongoDb and axios to fetch data from our API

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./TinderCards.css";
 import TinderCard from "react-tinder-card";
+import axios from "./axios";
+
 function TinderCards() {
   //useState hook to manage state
-  const [people, setPeople] = useState([
-    {
-      name: "Sachin Tendulkar",
-      url:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVJrTa7Oxf39xMTdlSo-JwzUYVFVWKo7nZCw&usqp=CAU",
-    },
-    {
-      name: "Shakrukh Khan",
-      url: "https://static.toiimg.com/photo/msid-69902898/69902898.jpg?115506",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+  //   {
+  //     name: "Sachin Tendulkar",
+  //     url:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVJrTa7Oxf39xMTdlSo-JwzUYVFVWKo7nZCw&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Shakrukh Khan",
+  //     url: "https://static.toiimg.com/photo/msid-69902898/69902898.jpg?115506",
+  //   },
+  // ]);
 
   //utility functions
+
+  useEffect(
+    () => {
+      // effect
+      async function fetchData() {
+        const req = await axios.get("/tinder/cards");
+
+        setPeople(req.data);
+      }
+      fetchData();
+    },
+    [
+      /*input*/
+    ]
+  );
   const Swiped = (direction, nameToBeDeleted) => {
     console.log("removing: ", nameToBeDeleted);
   };
@@ -38,7 +55,7 @@ function TinderCards() {
             >
               {/* this div canbe rendered vis passing props  */}
               <div
-                style={{ backgroundImage: `url(${person.url})` }}
+                style={{ backgroundImage: `url(${person.imgURL})` }}
                 className="card"
               >
                 <h3> {person.name}</h3>
